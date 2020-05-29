840f3db3f4a41f66b68cdd8522073d980e799967
shivamvk <shivam.vk529@gmail.com>
2020-05-29 23:43:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home/Home";
+import Auth from "./Auth/Auth";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/auth" exact>
+          <Auth />
+        </Route>
+      </main>
+    </Router>
   );
 }
 
---
+++ src/Auth/Auth.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Auth = () => {
+    return <h2>Auth works!</h2>
+};
+
+export default Auth;
