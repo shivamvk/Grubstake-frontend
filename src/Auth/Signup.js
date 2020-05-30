05186fe61884ff6fb8e6ff5884ceb2c6b1f4d7ec
shivamvk <shivam.vk529@gmail.com>
2020-05-30 22:45:56 +0530

    update auth for desktop and mobile screens diff

--- src/Auth/Signup.js
+++ src/Auth/Signup.js
@@ -2,23 +2,56 @@ import React from "react";
 import Social from "./sections/Social";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
-import SignupForm from './sections/SignupForm';
-
+import SignupForm from "./sections/SignupForm";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 import "./Auth.scss";
 
 const Signup = () => {
-  return (
-    <Container>
-      <h1>Signup</h1>
-      <h6>
-        Already have a Grubstake Account?{" "}
-        <Link to="/login"><span className="blue-text">Login</span></Link>
-      </h6>
-      <SignupForm />
-      <hr className="hr-text" data-content="OR"></hr>
-      <Social />
-    </Container>
-  );
+  if (window.innerWidth < 768) {
+    return (
+      <Container>
+        <h1>Signup</h1>
+        <h6>
+          Already have a Grubstake Account?{" "}
+          <Link to="/login">
+            <span className="blue-text">Login</span>
+          </Link>
+        </h6>
+        <SignupForm />
+        <hr className="hr-text" data-content="OR"></hr>
+        <Social />
+      </Container>
+    );
+  } else {
+    return (
+      <Container>
+        <h1>Signup</h1>
+        <h6>
+          Already have a Grubstake Account?{" "}
+          <Link to="/login">
+            <span className="blue-text">Login</span>
+          </Link>
+        </h6>
+        <Row>
+          <Col md={5}>
+            <SignupForm />
+          </Col>
+          <Col md={2} className="d-flex justify-content-center">
+            <div class="wrapper">
+              <div class="line"></div>
+              <div class="wordwrapper">
+                <div class="word">OR</div>
+              </div>
+            </div>
+          </Col>
+          <Col md={5}>
+            <Social />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 };
 
 export default Signup;
--- src/Auth/sections/Social.js
+++ src/Auth/sections/Social.js
@@ -8,8 +8,9 @@ import FacebookButton from "../components/FacebookButton";
 import "./Social.scss";
 
 const Social = () => {
-  return (
-    <Container className="text-center">
+  let element;
+  if (window.innerWidth < 768) {
+    element = (
       <Row>
         <Col xs={0} md={2}></Col>
         <Col xs={12} md={4} className="d-flex justify-content-center">
@@ -20,6 +21,21 @@ const Social = () => {
         </Col>
         <Col xs={0} md={2}></Col>
       </Row>
+    );
+  } else {
+    element = (
+      <div className="social-buttons-desktop">
+        <div className="vertical-center">
+          <GoogleButton />
+          <FacebookButton />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <Container className="text-center">
+      {element}
       <p className="text-muted">
         *By continuing you agree to our{" "}
         <span className="blue-text">Terms of service </span>
