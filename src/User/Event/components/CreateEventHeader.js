c28064b4b719c6a378fc6ecc7e42dea89560822b
shivamvk <shivam.vk529@gmail.com>
2020-06-03 03:06:18 +0530

    fix minor bugs

--- src/User/Event/components/CreateEventHeader.js
+++ src/User/Event/components/CreateEventHeader.js
@@ -3,20 +3,21 @@ import { FiChevronLeft as LeftIcon } from "react-icons/fi";
 import { Link, useHistory } from "react-router-dom";
 
 const CreateEventHeader = () => {
-    let history = useHistory();
+  let history = useHistory();
   return (
     <>
       <LeftIcon
         onClick={() => history.goBack()}
         className="font-size-lg back-icon-pos"
       />
-      <Link to="/">
-        <img
-          src="/g_logo.jpg"
-          className="main-navigation__logo logo-center-pos"
-          alt="g"
-        />{" "}
-      </Link>
+      <div className="main-navigation__title main-navigation__title_center top-center">
+        <Link to="/">
+          <img src="/g_logo.jpg" className="main-navigation__logo" alt="g" />
+          {window.innerWidth < 768 ? null : (
+            <span className="main-navigation__title-text">Grubstake</span>
+          )}
+        </Link>
+      </div>
     </>
   );
 };
